276979ab35853fe8fd01f324cdf48bd9ba488ad8
kalpeshbeit <patel.kalpeshbeit2010@gmail.com>
2017-06-03 15:47:32 +0530

    new project

--- js/config.js
+++
@@ -1,9 +0,0 @@
-
-    var krms_config = {
-        'ApiUrl': "http://callabhi.com/aicogapi/v1/index.php?action=",
-        'ServerUrl': "http://callabhi.com/aicogapi",
-        'append_api': "format/json",
-        'DialogDefaultTitle': "Aicogapi",
-        'pushNotificationSenderid': "684191859302",
-        'facebookAppId': "796560520473152"
-    };
